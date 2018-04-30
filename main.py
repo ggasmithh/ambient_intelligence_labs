f95cf9a7680c8da1d24e9855374458161234418d
Garrett Smith <ggasmithh@users.noreply.github.com>
2018-04-30 16:16:30 +0200

    Initial Commit
    Only showTasks is functional (early and often)

---
+++ main.py
@@ -0,0 +1,73 @@
+# excellent resource on pymysql
+# https://o7planning.org/en/11463/connecting-mysql-database-in-python-using-pymysql
+
+import pymysql.cursors
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# connect to our bot with our API token
+updater = Updater(token='547869170:AAGthxc9Ny967Qx6j4rZsOcSf4pLWWlg1qc')
+
+# just to make it easier to type since we will be using it a lot
+dispatcher = updater.dispatcher
+
+
+# handle the /start command
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Hi! I can help you manage a ToDo list!")
+
+
+# let the dispacher know about our new handler
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+# start polling for events
+updater.start_polling()
+
+
+# set up handling of other commands
+def echo(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+
+
+echo_handler = MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler)
+
+
+# gets our connection to the database
+def getConnection():
+    connection = pymysql.connect(host='localhost',
+                                 user='user',
+                                 password='passwd',
+                                 db='python-lab4',
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
+    return connection
+
+
+# function to show all tasks, or return a message if there are none
+def showTasks(bot, update):
+    connection = getConnection()
+
+    sql = "SELECT todo FROM task;"
+
+    try:
+        cursor = connection.cursor()
+        cursor.execute(sql)
+
+        tasks = []
+
+        for row in cursor:
+            tasks.append(row["todo"])
+
+        if not tasks:
+            bot.send_message(chat_id=update.message.chat_id, text="No tasks found")
+
+        else:
+            bot.send_message(chat_id=update.message.chat_id, text=('\n'.join(tasks)))
+
+    finally:
+        connection.close()
+
+
+showTasks_handler = CommandHandler('showTasks', showTasks)
+dispatcher.add_handler(showTasks_handler)
